ecd9e66798235e554297c05ae3b1dd66438bb587
Gityuhi <yuhiprivate@gmail.com>
2025-07-30 00:24:47 +0900

    商品管理機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :products
+  end
   resources :products
+
+  # get 'admin/products', to: 'admin#index'
+  # get 'admin/products/new', to: 'admin#new'
+  # post 'admin/products', to: 'admin#create'
+  # get 'admin/products/:id/edit', to: 'admin#edit'
+  # patch 'admin/products/:id', to: 'admin#update'
+  # delete 'admin/products/:id', to: 'admin#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
